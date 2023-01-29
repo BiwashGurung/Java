b9b4ebdef6fb4ec5fb3512b8b1aafe687e06a22c
Biwash Gurung <121740127+BiwashGurung@users.noreply.github.com>
2023-01-29 09:19:05 +0545

    Add files via upload

---
+++ BackwardDoWhileLoop.java
@@ -0,0 +1,21 @@
+
+/**
+ * Write a description of class BackwardDoWhileLoop here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class BackwardDoWhileLoop
+{
+    public static void main(String args[])
+    {
+        int a=100;
+        do{
+            System.out.println(a);
+            a=a-1;
+        }
+        
+        while(a>=1);
+        
+    }
+    }
