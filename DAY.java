e53268e6b7863e843bcef1f29fc223a071f9dbd4
Biwash Gurung <121740127+BiwashGurung@users.noreply.github.com>
2023-01-30 10:47:18 +0545

    Add files via upload

---
+++ DAY.java
@@ -0,0 +1,45 @@
+
+/**
+ * Write a description of class DAY here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class DAY
+{
+    public static void main(String [] args)
+    {
+     int day=7; 
+    switch(day){
+    case 0:
+        break;
+    case 7:
+        System.out.println("It's Sunday");
+      break;
+    case 1:
+      System.out.println("It's Monday");
+      break;
+    case 2:
+      System.out.println("It's Tuesday");
+      break;
+    case 3:
+      System.out.println("It's Wednesday");
+      break;
+    case 4:
+      System.out.println("It's Thursday");
+      break;
+    case 5:
+      System.out.println("It's Friday");
+      break;
+    case 6:
+      System.out.println("It's Saturday");
+      break;
+    default: 
+      System.out.println("Wrong input!");
+      break;
+
+  
+
+}
+}
+}
