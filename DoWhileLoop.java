8eb0b97a206159ed5b445ec4aafb231e354fbf0e
Biwash Gurung <121740127+BiwashGurung@users.noreply.github.com>
2023-01-31 08:12:04 +0545

    Add files via upload

---
+++ DoWhileLoop.java
@@ -0,0 +1,21 @@
+
+/**
+ * Write a description of class DoWhileLoop here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class DoWhileLoop
+{
+    public static void main(String[]args){
+        int i=1;
+        do{
+            System.out.println(i);
+            i++;
+        }
+        while (i<=100);
+        
+    }
+   }
+   
+   
