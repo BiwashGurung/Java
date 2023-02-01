83343dc70447f5df32c2e3cac615522609ad6b05
Biwash Gurung <121740127+BiwashGurung@users.noreply.github.com>
2023-02-01 12:16:25 +0545

    Add files via upload

---
+++ DoWhileLoopMultiply.java
@@ -0,0 +1,22 @@
+
+/**
+ * Write a description of class DoWhileLoopMultiply here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class DoWhileLoopMultiply
+{
+    public static void main(String [] args)
+    {
+      int a=1;
+      do{
+          System.out.println("2 X "+a+ " = "+(2*a));
+          a=a=1;
+      }
+      while(a<=10);
+    }
+}
+    
+        
+        
