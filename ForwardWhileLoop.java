5b600aacd30967b7d4a16e9d736b331d822544fa
Biwash Gurung <121740127+BiwashGurung@users.noreply.github.com>
2023-02-02 09:16:42 +0545

    Add files via upload

---
+++ ForwardWhileLoop.java
@@ -0,0 +1,18 @@
+
+/**
+ * Write a description of class ForwardWhileLoop here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class ForwardWhileLoop
+{
+   public static void main(String[] args){
+       int i=99;
+       while (i>=1){
+           System.out.println(i);
+           i=i-2;
+       }
+       
+   }
+}
