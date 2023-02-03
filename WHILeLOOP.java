c1ccfd165ba5c8420320cf6672f2c8fdaa347c4a
Biwash Gurung <121740127+BiwashGurung@users.noreply.github.com>
2023-02-03 10:28:53 +0545

    Add files via upload

---
+++ WHILeLOOP.java
@@ -0,0 +1,19 @@
+
+/**
+ * Write a description of class WHILeLOOP here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class WHILeLOOP
+{
+    public static void main(String[] args)
+    {
+        int i=1;
+        while(i<=100){
+            System.out.println(i);
+            i++;
+        }
+        
+    }
+   }
