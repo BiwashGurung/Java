6a9499b2b8e54d957bc1fe478b52e4fec89f88c3
Biwash Gurung <121740127+BiwashGurung@users.noreply.github.com>
2023-02-04 09:15:55 +0545

    Add files via upload

---
+++ WhileLoopBackward.java
@@ -0,0 +1,19 @@
+
+/**
+ * Write a description of class WhileLoopBackward here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class WhileLoopBackward
+{
+    public static void main(String[]args){
+
+        int i=100;
+        while(i>=1){
+            System.out.println(i);
+            i=i-1;
+
+        }
+}
+}
