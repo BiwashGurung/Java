4cb552ca1c5e8c052b7053bc7b82b3d5da6efb66
Biwash Gurung <121740127+BiwashGurung@users.noreply.github.com>
2023-02-05 10:47:05 +0545

    Add files via upload

---
+++ Maths.java
@@ -0,0 +1,21 @@
+
+/**
+ * Write a description of class Maths here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class Maths
+{
+    public static void main(String[] args)
+    {
+        int a=6;
+        int b=9;
+        int addition =a+b;
+        System.out.println("The addition is:"+addition);
+    }
+
+
+
+
+}
