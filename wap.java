3d6b531e77e5d0e28c29fcbc281b815dcd7f0c7e
Biwash Gurung <121740127+BiwashGurung@users.noreply.github.com>
2023-02-06 12:23:19 +0545

    Add files via upload

---
+++ wap.java
@@ -0,0 +1,24 @@
+
+/**
+ * Write a description of class wap here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class wap
+{
+   public static void main(String[] args)
+   {
+       int a=1;
+       int b=2;
+       if(a>b){
+           System.out.println("The greatest number is "+a);
+        }
+    else{
+        System.out.println("The greatest number is "+b);
+    }
+           
+       }
+       
+   }
+
