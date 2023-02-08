d4f29adbf69b5fbf50e7e883aa4b71039044704d
Biwash Gurung <121740127+BiwashGurung@users.noreply.github.com>
2023-02-08 09:01:17 +0545

    Add files via upload

---
+++ Animal.java
@@ -0,0 +1,17 @@
+
+/**
+ * Write a description of class Animal here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class Animal
+{
+    public int legs;
+    
+    public void display(){
+        System.out.println(legs);
+    }
+    
+}
+
