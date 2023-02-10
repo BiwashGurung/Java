4493bdc3d521126c5a4382108af491ae25247d21
Biwash Gurung <121740127+BiwashGurung@users.noreply.github.com>
2023-02-10 09:27:43 +0545

    Add files via upload
    I have used number in class but It is illegal to use in real world. This is only for practise purpose

---
+++ Array0.java
@@ -0,0 +1,16 @@
+
+/**
+ * Write a description of class Array0 here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class Array0
+{
+   public static void main(String [] args)
+   {
+       String[] array= new String[5];
+       array[0]="Ram";
+       System.out.println(array[0]);
+   }
+}
