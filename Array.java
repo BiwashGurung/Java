47f8e9d39cd5fa58c6ffdd12e58b152ed48c562f
Biwash Gurung <121740127+BiwashGurung@users.noreply.github.com>
2023-02-10 09:25:27 +0545

    Add files via upload

--- Array.java
+++ Array.java
@@ -6,32 +6,12 @@
  * @version (a version number or a date)
  */
 public class Array
-{   
-    public int[] arr;
-    
-    public double sum(){
-        double sum=0;
-        for(int i = 0; i<arr.length; i++){
-            sum=arr[i]+sum;
-        }
-        return sum;
+{
+    public static void main(String [] args)
+    {
+        String[] students=new String[5];
+        students[0]="Ram";
+        students[0]="shyam"; //new value will be updated even the values are same
+        System.out.println(students[0]);
     }
-
-    public double average(){
-        
-        double av = sum()/arr.length;
-        return av;
-    }
-
-    public void printEachOfArray(){
-        for(int i = 0; i<arr.length; i++){
-            System.out.println(arr[i]);
-        }
-    }
-    
-     public void display(String str,double value){   
-        System.out.println(str+value);
-    }
-    
 }
-
