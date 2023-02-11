4d5c4cb16c6924b8d66e9184e59e35bcafd544c3
Biwash Gurung <121740127+BiwashGurung@users.noreply.github.com>
2023-02-11 08:37:32 +0545

    Add files via upload

---
+++ Array2D.java
@@ -0,0 +1,16 @@
+
+/**
+ * Write a description of class Array2D here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class Array2D
+{
+   public static void main(String[] args)
+   {
+       int[] [] array={{1,2} , {3,4}};
+       System.out.println(array[1] [0]);
+       
+   }
+}
