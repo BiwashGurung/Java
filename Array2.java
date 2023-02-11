ff7ad740b5cfefe37d2d7077681db85d4446c101
Biwash Gurung <121740127+BiwashGurung@users.noreply.github.com>
2023-02-11 08:37:13 +0545

    Add files via upload

---
+++ Array2.java
@@ -0,0 +1,17 @@
+
+/**
+ * Write a description of class Array2 here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class Array2
+{
+   public static void main(String [] args)
+   {
+       String[] students={"Ram","Shyam"};
+       for(int i=0; i<2; i++){
+           System.out.println(students[i]);
+       }
+   }
+}
