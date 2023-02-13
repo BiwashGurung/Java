5bf0cde7f663f2c90c1d7501dd0fe29c2b15eb10
Biwash Gurung <121740127+BiwashGurung@users.noreply.github.com>
2023-02-13 19:32:44 +0545

    Add files via upload

---
+++ DuplicateElement.java
@@ -0,0 +1,23 @@
+
+/**
+ * Write a description of class DuplicateElements here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+
+public class DuplicateElement {  
+    public static void main(String[] args) {  
+            //Initialize array  
+            int [] arr = new int [] {1, 2, 3, 4, 2, 7, 8, 8, 3,9,1,6};  
+            System.out.println("Duplicate elements in given array: ");  
+            //Searches for duplicate element  
+            for(int i = 0; i < arr.length; i++) {  
+                for(int j = i + 1; j < arr.length; j++) {  
+                    if(arr[i] == arr[j])  
+                        System.out.println(arr[j]);  
+                }  
+            }  
+    }  
+}  
+
