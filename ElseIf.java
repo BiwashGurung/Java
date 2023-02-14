51a6d88ce553e4e7a29ba3736d6cc622dafde602
Biwash Gurung <121740127+BiwashGurung@users.noreply.github.com>
2023-02-14 18:48:30 +0545

    Add files via upload

---
+++ ElseIf.java
@@ -0,0 +1,24 @@
+
+/**
+ * Comparing two numbers using else-if.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class ElseIf
+{
+    public static void main(String[] args)
+    {
+        //declaring two numbers for comparing
+        int numberOne = 44;
+        int numberTwo = 40;
+        if(numberOne > numberTwo){
+            System.out.println(numberOne + " is greater than " +numberTwo);
+        }else if(numberOne < numberTwo){
+            System.out.println(numberOne + " is less than " +numberTwo);
+        }else{
+            System.out.println(numberOne + " is equal to " +numberTwo);
+        }
+        }
+}
+
