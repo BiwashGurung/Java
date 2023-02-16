cb69f86d21af6d708ca7613744943ef908c250da
Biwash Gurung <121740127+BiwashGurung@users.noreply.github.com>
2023-02-16 19:15:18 +0545

    Add files via upload

---
+++ Arithmetic_operators.java
@@ -0,0 +1,32 @@
+
+/**
+ * Write a description of class Arithmetic here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class Arithmetic_operators{
+    public static void main(String as[])
+    {
+        int a, b, c;
+        a=30;
+        b=21;
+        c=a+b;
+        System.out.println("Addtion: "+c);
+        c=a-b;
+        System.out.println("Substraction: "+c);
+        c=a*b;
+        System.out.println("Multiplication: "+c);
+        c=a/b;
+        System.out.println("Division: "+c);
+        b=3;
+        c=a%b;
+        System.out.println("Remainder: "+c);
+        a=++a;
+        System.out.println("Increment Operator: "+a);
+        a=--a;
+        System.out.println("decrement Operator: "+a);       
+
+    }
+}
+    
