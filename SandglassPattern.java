5ccdd3511486d66d545ab96c5a0c8f75ef0b1046
Biwash Gurung <121740127+BiwashGurung@users.noreply.github.com>
2023-02-20 18:38:55 +0545

    Add files via upload

---
+++ SandglassPattern.java
@@ -0,0 +1,39 @@
+
+/**
+ * Write a description of class Sandglass here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class SandglassPattern
+{
+    public static void main(String[] args)   
+    {  
+        int n = 5;    
+        for (int i = 1; i <= n; i++)   
+        {  
+            for (int j = 1; j < i; j++)   
+            {  
+            System.out.print(" ");  
+            }  
+            for (int k = i; k <= n; k++)   
+            {   
+            System.out.print(k+" ");  
+            }  
+            System.out.println();  
+        }  
+        for (int i = n-1; i >= 1; i--)   
+        {  
+            for (int j = 1; j < i; j++)   
+            {  
+            System.out.print(" ");  
+            }  
+            for (int k = i; k <= n; k++)  
+            {  
+            System.out.print(k+" ");  
+            }  
+            System.out.println();  
+        }  
+    }  
+}  
+
