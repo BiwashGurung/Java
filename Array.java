3cfcf4de9d9a72b64711908e811c994851b5f77e
Biwash Gurung <121740127+BiwashGurung@users.noreply.github.com>
2023-02-21 19:14:51 +0545

    Add files via upload

--- Array.java
+++ Array.java
@@ -1,17 +1,36 @@
 
 /**
- * Write a description of class Array here.
+ * Printing 1-100 without using the loop.
  *
  * @author (your name)
  * @version (a version number or a date)
  */
 public class Array
 {
-    public static void main(String [] args)
-    {
-        String[] students=new String[5];
-        students[0]="Ram";
-        students[0]="shyam"; //new value will be updated even the values are same
-        System.out.println(students[0]);
-    }
+   public static void main(String[] args)
+   {
+       int [] array =new int [100];
+       display(array, 0, 1);
+   }
+   public static void display(int[] array, int index, int number){
+       array[index]= number;
+       System.out.println(array[index]);
+       index++;
+       number++;
+       if(index < array.length){
+             display(array, index, number);
+       }
+   }
 }
+ // public static void main(String[] args)
+    // {
+        // int[] array = {1, 2, 3};
+        // display(array, 0);
+    // }
+    // public static void display(int[] array, int index){
+        // System.out.println(array[index]);
+        // index++;
+        // if(index < array.length){
+            // display(array, index);
+        // }
+    // }
