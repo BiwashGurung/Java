6f9b547e610077274b21ed620e5e77d5c984ebdd
Biwash Gurung <121740127+BiwashGurung@users.noreply.github.com>
2023-02-23 19:13:54 +0545

    Add files via upload

---
+++ A.java
@@ -0,0 +1,13 @@
+
+/**
+ * Write a description of class A here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class A
+{
+   public void show1(){
+        System.out.println("Parent class A");
+    }
+}
---
+++ B.java
@@ -0,0 +1,13 @@
+
+/**
+ * Write a description of class B here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class B extends A
+{
+    public void show2(){
+        System.out.println("Child class B");
+    }
+}
