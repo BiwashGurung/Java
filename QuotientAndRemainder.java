10886a7c5bd216acb7e714d4a021ccf69de2ae77
Biwash Gurung <121740127+BiwashGurung@users.noreply.github.com>
2023-02-25 19:33:59 +0545

    Add files via upload

---
+++ QuotientAndRemainder.java
@@ -0,0 +1,16 @@
+
+/**
+ * Program to find quotient and remainder.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class QuotientAndRemainder {
+    public static void main(String[] args) {
+        int num1 = 25, num2 = 4;
+        int quotient = num1 / num2;
+        int remainder = num1 % num2;
+        System.out.println("Quotient is: " + quotient);
+        System.out.println("Remainder is: " + remainder);
+    }
+}
