3ecba954b246039dca94c033219fa8ffab1da62f
Biwash Gurung <121740127+BiwashGurung@users.noreply.github.com>
2023-02-25 19:35:05 +0545

    Add files via upload

---
+++ PrimeNumbers.java
@@ -0,0 +1,40 @@
+
+/**
+ * Program to find the prime number using scanner.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+import java.util.Scanner;
+class PrimeNumbers
+{
+   public static void main (String[] args)
+   {		
+      Scanner scanner = new Scanner(System.in);
+      int i =0;
+      int num =0;
+      //Empty String
+      String  primeNumbers = "";
+      System.out.println("Enter the value of n:");
+      int n = scanner.nextInt();
+      scanner.close();
+      for (i = 1; i <= n; i++)  	   
+      { 		 		  
+         int counter=0; 		  
+         for(num =i; num>=1; num--)
+         {
+	    if(i%num==0)
+	    {
+		counter = counter + 1;
+	    }
+	 }
+	 if (counter ==2)
+	 {
+	    //Appended the Prime number to the String
+	    primeNumbers = primeNumbers + i + " ";
+	 }	
+      }	
+      System.out.println("Prime numbers from 1 to n are :");
+      System.out.println(primeNumbers);
+   }
+}
