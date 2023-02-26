af09f4878e434f5f75fe040050523584dbd77a70
Biwash Gurung <121740127+BiwashGurung@users.noreply.github.com>
2023-02-26 20:32:22 +0545

    Add files via upload

---
+++ ConcatenationExamples.java
@@ -0,0 +1,19 @@
+
+/**
+ * Write a description of class s here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class ConcatenationExamples
+{
+   public static void main(String[] args)
+   {
+       String a = "Hello";
+       String b = " I am Dexy.";
+       //Using "+" operator
+       String c = a + b;
+       //Printing
+       System.out.println(c);
+   }
+}
