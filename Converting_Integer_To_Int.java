359f0ba4305dd94a090149be0c6a43de5e109031
Biwash Gurung <121740127+BiwashGurung@users.noreply.github.com>
2023-03-01 21:26:01 +0545

    Add files via upload

---
+++ Converting_Integer_To_Int.java
@@ -0,0 +1,16 @@
+
+/**
+ * Write a description of class hehe here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class Converting_Integer_To_Int {
+    public static void main(String[] args) {
+        Integer myInt = new Integer(2000);
+        System.out.println("Integer value = " + myInt);
+        //converting Integer to int
+        int newInt = myInt;
+        System.out.println("int value = " + newInt);
+    }
+}
