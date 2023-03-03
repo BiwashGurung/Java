94e9c6efc9f93968d2e481cb783ad25f3ea46911
Biwash Gurung <121740127+BiwashGurung@users.noreply.github.com>
2023-03-03 19:57:14 +0545

    Add files via upload

---
+++ OctalToHexadecimal.java
@@ -0,0 +1,35 @@
+//Conversion From Octal To Hexadecimal Java Program Using standard Method
+
+import java.util.Scanner;
+class OctalToHexadecimal
+{
+    public static void main(String arg[])
+    {
+        int oct, dec=0, i=0, t;
+        Scanner sc = new Scanner(System.in);		
+        System.out.println("Enter Octal Number : ");
+        oct = sc.nextInt();				
+        while(oct != 0)//when octal is not equals to 0
+        {
+            dec =dec + (oct%10) *(int)Math.pow(8, i);
+            i++;
+            oct = oct/10;
+        }		     
+        String hex=hexdecimal(dec); 
+        System.out.println("Hexdecimal number is :"+hex);   
+    }
+
+    public static String hexdecimal(int q)
+    {
+        char a[]={'0','1','2','3','4','5','6','7','8','9','A','B','C','D','E','F'};
+        int rem;
+        String hexdec="";
+        while(q != 0)
+        {
+            rem=q%16;
+            hexdec= a[rem] + hexdec;
+            q= q/16;
+        }
+        return hexdec;
+    }
+}
