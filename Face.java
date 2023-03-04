81e4ed0eedd9ada789e1a59ec18d4bcd66f85d20
Biwash Gurung <121740127+BiwashGurung@users.noreply.github.com>
2023-03-04 21:01:28 +0545

    Add files via upload

---
+++ Face.java
@@ -0,0 +1,19 @@
+
+/**
+ * Write a description of class ann here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class Face 
+{
+    public static void main(String[] args)
+    {
+        System.out.println(" +\"\"\"\"\"+ ");
+        System.out.println("{| o o |}");
+        System.out.println(" |  ^  | ");
+        System.out.println(" |  -  | ");
+        System.out.println(" +-----+ ");
+    }
+
+}
