c2246a73803f2ce6d678d5416b8fd2885445585a
Biwash Gurung <121740127+BiwashGurung@users.noreply.github.com>
2023-03-19 14:25:31 +0545

    Add files via upload

--- JavaStudent.java
+++ JavaStudent.java
@@ -1,15 +1,8 @@
-
-/**
- * Write a description of class JavaStudent here.
- *
- * @author (your name)
- * @version (a version number or a date)
- */
-abstract class JavaStudent
-{
-    public void learnJava(){
+abstract class JavaStudent {
+    public void learnjava() {
         System.out.println("I am learning java");
     }
+    
     public abstract void name();
     public abstract void location();
 }
