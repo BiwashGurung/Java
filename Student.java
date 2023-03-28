7641bc10d5e8ecdc24cd55d213924d215164245c
Biwash Gurung <121740127+BiwashGurung@users.noreply.github.com>
2023-03-28 08:56:40 +0545

    Add files via upload

--- Student.java
+++ Student.java
@@ -1,36 +1,49 @@
 
-/**
- * Write a description of class Student here.
- *
- * @author (your name)
- * @version (a version number or a date)
- */
-public class Student
-{
-   public static void main(String[] args)
-   {
-       String name="Biwash Gurung";
-       int rollNumber=5;
-       boolean isPresent =true;
-       float lectureHours=2.5f;
-       String address="Gharipatan";
-       String gender="Male";
-       String module="Programming";
-       
-       System.out.println("My name is"+name);
-       System.out.println("My roll number is"+rollNumber);
-       System.out.println("My prensent is"+isPresent);
-       if(isPresent){
-           System.out.println("I am Present");
-        }else{
-            System.out.println("I am Absent");
-        }
-        System.out.println("The lecture hour is"+lectureHours);
-        System.out.println("My address is"+address);
-        System.out.println("My gender is"+gender);
-        System.out.println("Subject module"+module);
-        
-        
-        
-   }
+import java.awt.*;
+
+public class Student {
+    private String name;
+    private String age;
+    private String address;
+    private String phone;
+
+    public Student(String name, String age, String address, String phone) {
+        this.name = name;
+        this.age = age;
+        this.address = address;
+        this.phone = phone;
+    }
+
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setAge(String age) {
+        this.age = age;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getAge() {
+        return age;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
 }
